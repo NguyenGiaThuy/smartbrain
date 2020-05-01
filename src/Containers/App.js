e118eab3a378757a82571b18a1982d176c3daa9c
NguyenGiaThuy <nguyengiathuy@gmail.com>
2020-05-01 19:17:29 +0700

    updating features

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from '../Components/Navigation';
 import Logo from '../Components/Logo';
@@ -18,19 +18,39 @@ const particleOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className='App'>
-      <Particles className='particles'
-        params={particleOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Particles className='particles'
+          params={ particleOptions }
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={ this.onInputChange }
+          onButtonSubmit={ this.onButtonSubmit }
+        />
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
